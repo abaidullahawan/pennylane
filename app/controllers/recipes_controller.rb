f565b13aa97475d24fe99a8eb5adcd423bb63677
Mohammad Haris <mohammad.haris@devbox.co>
2022-03-04 11:53:56 +0500

    most recent search added

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,15 +4,13 @@ require 'will_paginate/array'
 
 class RecipesController < ApplicationController
   def search_recipe
-    ingredient_search = search_params['search']&.gsub(',', ' ')&.split(' ')
-    @q = Ingredient.joins(:recipe).includes(:recipe)
-      .ransack(name_cont_any: ingredient_search, title_cont_any: ingredient_search)
-    @ingredients = search_params.present? ? @q.result.uniq(&:name) : Ingredient.all
-
+    ingredient_search = search_params['search']
+    @ingredients = ingredient_search.present? ?
+      Ingredient.search_ingredient(search_params['search']).limit(35) : Ingredient.first(1000)
     @ingredients = @ingredients.paginate page: params[:page]
   end
 
-  private
+private
     def search_params
       params.permit(:search)
     end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
 class Ingredient < ApplicationRecord
+  include PgSearch::Model
+  pg_search_scope :search_ingredient,
+                  associated_against: { recipe: [:title, :category] },
+                  against: { name: 'A' },
+                    using: {
+                      tsearch: {
+                        dictionary: 'english', tsvector_column: 'searchable'
+                      }
+                    }
+
   belongs_to :recipe
 end
