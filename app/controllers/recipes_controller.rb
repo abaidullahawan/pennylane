9c7793ef10176ccc3d1ff00743feeaaf063c64b0
Salman Tahir <salmantahir3249@gmail.com>
2022-03-04 22:48:19 +0500

    show page added with some condition

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,13 +2,22 @@
 
 class RecipesController < ApplicationController
   def search_recipe
-    ingredient_search = search_params['search']
-    @q = ingredient_search.present? ?
-      Ingredient.search_ingredient(search_params['search']).ransack(params[:q]) : Ingredient.ransack(params[:q])
-    @pagy, @ingredients = pagy(@q.result, items: 12)
+    ingredient_search = split_search_params(search_params)
+    @q = ingredient_search.blank? ? Ingredient.ransack(params[:q]) :
+      Ingredient.search_ingredient(search_params['search']).ransack(params[:q])
+    @pagy, @ingredients = pagy(@q.result.includes(:recipe), items: 12)
+  end
+
+  def show_ingredient
+    @ingredient = Ingredient.find(params[:id])
   end
 
   private
+    def split_search_params(search_params)
+      ingredient_search = search_params['search']&.split(' ')
+      ingredient_search&.select { |t| t.size >= 3 }
+    end
+
     def search_params
       params.permit(:search)
     end
